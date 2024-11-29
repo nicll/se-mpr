709a881ff3747e894f9e7aff396077433773d180
Nicolas Klement <nicolas.klement@gmail.com>
2024-11-29 21:59:56 +0100

    Add service configuration code for ADO.NET and MongoDB clustering

--- OrleansPrototype/OrleansPrototype.SiloService/Program.cs
+++ OrleansPrototype/OrleansPrototype.SiloService/Program.cs
@@ -2,6 +2,33 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Host.UseOrleans(silo =>
+{
+    silo.UseLocalhostClustering();
+
+    // configure ADO.NET SQL server clustering
+    //silo.UseAdoNetClustering(opts =>
+    //{
+    //    opts.Invariant = "System.Data.SqlClient";
+    //    opts.ConnectionString = @"Data Source=(localdb)\SQLEXPRESS;" +
+    //        "Initial Catalog=mpr_orleans_a;Integrated Security=True;" +
+    //        "Pooling=False;Max Pool Size=200;" +
+    //        "Asynchronous Processing=True;MultipleActiveResultSets=True";
+    //});
+
+    // configure MongoDB client
+    //silo.UseMongoDBClient("mongodb://localhost:27017/");
+
+    // configure Orleans to use MongoDB client
+    //silo.UseMongoDBClustering(opts =>
+    //{
+    //    opts.DatabaseName = "mpr_orleans_a";
+    //    opts.Strategy = Orleans.Providers.MongoDB.Configuration.MongoDBMembershipStrategy.SingleDocument;
+    //});
+
+    silo.ConfigureLogging(log => log.AddConsole());
+});
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
